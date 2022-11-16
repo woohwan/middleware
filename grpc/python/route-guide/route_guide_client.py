2553f61be8bb867a4ac123167d70e77d956bbc69
woohwan <whpark70@naver.com>
2022-11-16 09:06:17 +0000

    route_guide #1

---
+++ grpc/python/route-guide/route_guide_client.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+
+import asyncio
+import logging
+import random
+from typing import Iterable, List
+
+import grpc
+import route_guide_pb2
+import route_guide_pb2_grpc
+import route_guide_resources
+
+def make_route_note(message, latitude, longitue):
+  return route_guide_pb2.RouteNote(
+    message=message,
+    location=route_guide_pb2.Point(
+      latitude=latitude,
+      longitude=longitue
+    )
+  )
+
+def generate_message() -> Iterable[route_guide_pb2.RouteNote]:
+  messages = [
+    make_route_note("1st message", 0,0),
+    make_route_note("2nd message", 0,1),
+    make_route_note("3rd message", 1,0),
+    make_route_note("4th message", 0,0),
+    make_route_note("5st message", 1,0),
+  ]
+  for msg in messages:
+    print(f"Sending %s at %s" % (msg.message, msg.location))
+    yield msg
+
+# Performs a bidi-streaming
+def guide_route_chat(stub):
+  responses = stub.RouteChat(generate_message())
+  for response in responses:
+    print("Received message %s at %s" %
+              (response.message, response.location))
+
+
+def run():
+  with grpc.insecure_channel('localhost:50051') as channel:
+    stub = route_guide_pb2_grpc.RouteGuideStub(channel)
+    print("------------------ RouteChat -------------")
+    guide_route_chat(stub)
+
+if __name__ == "__main__":
+  logging.basicConfig()
+  run()
